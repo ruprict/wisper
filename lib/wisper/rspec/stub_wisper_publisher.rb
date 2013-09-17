91f50b92d39c0f8700cb5f70aaee1d78669d3f52
Glenn Goodrich <glenn.goodrich@gmail.com>
2013-09-17 15:24:49 -0400

    Add stub_wisper_publisher_with_args

--- lib/wisper/rspec/stub_wisper_publisher.rb
+++ lib/wisper/rspec/stub_wisper_publisher.rb
@@ -13,3 +13,11 @@ def stub_wisper_publisher(clazz, called_method, event_to_publish, *published_eve
     end
   end)
 end
+
+def stub_wisper_publisher_with_args(clazz, called_method, event_to_publish, *published_event_args)
+  stub_const(clazz, Class.new(TestWisperPublisher) do
+    define_method(called_method) do |*args|
+      publish(event_to_publish, *published_event_args)
+    end
+  end)
+end
--- spec/lib/rspec_extensions_spec.rb
+++ spec/lib/rspec_extensions_spec.rb
@@ -11,6 +11,23 @@ describe Wisper do
         end
         publisher.execute
       end
+
+      def do_something_args(arg1)
+        publisher = ArgsPublisher.new
+        
+        publisher.on(:some_event) do |variable1|
+          return "Hello with #{variable1}!"
+        end
+        publisher.execute("bar")
+      end
+    end
+
+    class ArgsPublisher
+      include Wisper::Publisher
+
+      def execute(an_arg) 
+        broadcast(:some_event, an_arg)
+      end
     end
 
     context "when stubbing the publisher to emit an event" do
@@ -22,6 +39,21 @@ describe Wisper do
         response = CodeThatReactsToEvents.new.do_something
         response.should == "Hello with foo1 foo2!"
       end
+    end
+
+    context "when stubbing a publisher that takes arguments" do
+      before do
+        stub_wisper_publisher_with_args("ArgsPublisher", :execute, :some_event, "foo1", "foo2")
+      end
+
+      it "emits the event" do
+        response = CodeThatReactsToEvents.new.do_something_args("bar")
+
+        response.should == "Hello with foo1!"
+      end
+    
+     
+    
     end
   end
 end
